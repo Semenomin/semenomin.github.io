3a10c71bdc3f82a6f866780e5b177a1f57c70c80
Semenomin <straikermc999@gmail.com>
2018-12-07 01:26:58 +0300

    1.12

--- JS.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('#carousel').carousel({
-        interval: 10000,
-        keyboard: false,
-        pause: 'hover',
-        ride: 'carousel',
-        wrap: false
-    });
-});
---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(function() {
+
+    $(window).scroll(function() {
+
+        if($(this).scrollTop() != 0) {
+
+            $('#toTop').fadeIn();
+
+        } else {
+
+            $('#toTop').fadeOut();
+
+        }
+
+    });
+
+    $('#toTop').click(function() {
+
+        $('body,html').animate({scrollTop:0},800);
+
+    });
+
+});
+
+$(document).ready(function(){
+
+    $("#menu").on("click","a", function (event) {
+
+//отменяем стандартную обработку нажатия по ссылке
+
+        event.preventDefault();
+//збираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+//узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+$(document).ready(function(){
+
+    $(".header").on("click","a", function (event) {
+
+//отменяем стандартную обработку нажатия по ссылке
+
+        event.preventDefault();
+//збираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+//узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
