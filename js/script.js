d436d56f616f51ddb94da7b7080007d6026db448
Semenomin <straikermc999@gmail.com>
2018-12-12 11:08:37 +0300

    final

--- js/script.js
+++ js/script.js
@@ -41,14 +41,12 @@ $(document).ready(function(){
 $(document).ready(function(){
 
     $(".header").on("click","a", function (event) {
-
 //отменяем стандартную обработку нажатия по ссылке
-
         event.preventDefault();
 //збираем идентификатор бока с атрибута href
         var id  = $(this).attr('href'),
 //узнаем высоту от начала страницы до блока на который ссылается якорь
-            top = $(id).offset().top;
+        top = $(id).offset().top;
         //анимируем переход на расстояние - top за 1500 мс
         $('body,html').animate({scrollTop: top}, 1500);
     });
